d57f817e9c6c09da7e1143a49223f94f31d0903e
Stefano Palomino <stefanop21@outlook.es>
2024-11-07 16:33:51 -0500

    feat(interfaces): added hotel interface

---
+++ src/interfaces/hotel.ts
@@ -0,0 +1,13 @@
+export interface Hotel {
+  id: number | string
+  name: string
+  phoneContact: string
+  address: string
+  email: string
+  img: string
+  rating: number
+  ratingCount: number
+  minPrice: number
+  maxPrice: number
+  url: string
+}
